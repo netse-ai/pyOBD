675746d5565a97a2513ca4648b3d5c358ed432e8
tear727 <tear728@gmail.com>
2019-08-12 15:55:40 -0700

    c

--- obdi/elm327.py
+++ obdi/elm327.py
@@ -1,32 +1,44 @@
 import serial
 import time
 import threading
+import os
+import stat
 from commands import commands
+from serial import SerialException
 
 class ELM327(object):
     def __init__(self, baudrate, timeout):
         self.baudrate = baudrate
         self.timeout = timeout
         self.ser = None
-        
+
     def _connect(self):
-        if self.ser != None:
+        print "Attempting to Connect"
+        if self.ser == None:
             print "Not None"
-            self.ser = serial.Serial('dev/rfcomm0', baudrate=self.baudrate, timeout=self.timeout)
-            if self.ser.isOpen():
-                print "Connection with: {0} established".format(self.ser.name)
+	    try:
+                stat.S_ISBLK(os.stat("/dev/rfcomm0").st_mode)
+            except:
+                os.system("sudo rfcomm bind rfcomm0 00:1D:A5:02:86:67")
+            
+            try:
+                self.ser = serial.Serial('dev/rfcomm0', baudrate=self.baudrate, timeout=self.timeout)
+                if self.ser.isOpen():
+                    print "Connection with: {0} established".format(self.ser.name)
 
-                self.ser.write('ATZ\r')
-                self.ser.write('astp0\r')
-                self.ser.write('atho\r')
-                print "Testing Connection: ", self.ser.readline().split(' ')
-                time.sleep(1)
-                self.ser.flushInput()
-                self.ser.flushOutput()
-                print "Connection Succesful"
-            else:
+                    self.ser.write('ATZ\r')
+                    self.ser.write('astp0\r')
+                    self.ser.write('atho\r')
+                    print "Testing Connection: ", self.ser.readline().split(' ')
+                    time.sleep(1)
+                    self.ser.flushInput()
+                    self.ser.flushOutput()
+                    print "Connection Succesful"
+                else:
+                    print "error"
 
-                print "error"
+            except SerialException:
+                print "Error"
 
     def _command(self, cmd):
         if self.ser.isOpen():
