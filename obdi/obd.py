21cb0344fa80a518de837140f6f044307bec137d
Trent Resh <tear728@gmail.com>
2019-08-13 16:59:14 -0700

    elm

--- obdi/obd.py
+++ obdi/obd.py
@@ -2,11 +2,10 @@ from elm327 import ELM327
 
 class OBDI(object):
     def __init__(self):
-        self.interface = ELM327(38400, 0.5)
+        self.interface = ELM327(38400, 0.1)
 
     def connect(self):
         self.interface._connect()
 
     def cmd(self, cmd):
         return self.interface._command(cmd)
-
