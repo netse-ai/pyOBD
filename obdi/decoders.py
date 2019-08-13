2009391249df25c2a967c8687d3359f88e62863a
Trent Resh <tear728@gmail.com>
2019-08-13 16:44:26 -0700

    elm

--- obdi/decoders.py
+++ obdi/decoders.py
@@ -1,22 +1,22 @@
 def speed(val):
-    return "SPEED: ", float(int('0x'+ val, 0))
+    return float(int('0x'+ val, 0))
 
 def rpm(val):
     upper = float(int('0x'+ val[0:2], 0)) * 256
     lower = float(int('0x'+ val[2:4], 0))
-    return "RPM: ", (upper + lower) / 4
+    return (upper + lower) / 4
 
 def engine_load(val):
-    return "ENGINE_LOAD: ", float(int('0x'+ val, 0)) / 2.55
+    return float(int('0x'+ val, 0)) / 2.55
 
 def engine_coolant_temp(val):
-    return "ENGINE_COOLANT_TEMP: ", float(int('0x'+ val, 0)) - 40
+    return float(int('0x'+ val, 0)) - 40
 
 def short_term_fuel_trim(val):
-    return "SHORT_TERM_FUEL_TRIM: ", ((100/128)*float(int('0x'+ val, 0))) - 100
+    return ((100/128)*float(int('0x'+ val, 0))) - 100
 
 def fuel_pressure(val):
-    return "FUEL_PRESSURE: ", 3 * float(int('0x'+ val, 0))
+    return 3 * float(int('0x'+ val, 0))
 
 def intake_manifold_pressure(val):
-    return "INTAKE_PRESSURE", float(int('0x'+ val, 0))
+    return float(int('0x'+ val, 0))
