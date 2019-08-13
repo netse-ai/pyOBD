55ebdd2f37716e78e717fcc36cbf43b371e01192
Trent Resh <tear728@gmail.com>
2019-08-13 16:16:35 -0700

    elm

--- obdi/decoders.py
+++ obdi/decoders.py
@@ -4,5 +4,5 @@ def speed(val):
 def rpm(val):
     upper = float(int('0x'+ val[0:2], 0)) * 256
     lower = float(int('0x'+ val[2:4], 0))
-    print "worked"
+    print "worked: ", val
     return (upper + lower) / 4
