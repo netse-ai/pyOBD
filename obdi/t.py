b5d3b36fc256d40ab5b9d1dc298027df8a70bd16
Trent Resh <tear728@gmail.com>
2019-08-13 16:14:51 -0700

    elm

--- obdi/t.py
+++ obdi/t.py
@@ -4,7 +4,7 @@ obd = OBDI()
 obd.connect()
 print obd.cmd(commands["SPEED"])
 print obd.cmd(commands["RPM"])
-obd.cmd(commands["RPM"].print_decoder())
+commands["RPM"].print_decoder()
 # while True:
 #     print obd.cmd(commands["SPEED"]), obd.cmd(commands["RPM"])
     # cmds = {
