6da130536da7b1e873a873338fa431624924c218
Trent Resh <tear728@gmail.com>
2019-08-13 16:43:32 -0700

    elm

--- obdi/t.py
+++ obdi/t.py
@@ -2,7 +2,17 @@ from obd import OBDI
 from commands import commands
 obd = OBDI()
 obd.connect()
-print obd.cmd(commands["SPEED"]), obd.cmd(commands["RPM"]), obd.cmd(commands["ENGINE_LOAD"]), obd.cmd(commands["COOLANT_TEMP"])
+
+with open('speed_log.txt', 'w') as sl:
+    pass
+
+while True:
+    try:
+        print "----------------"
+        print obd.cmd(commands["SPEED"]), obd.cmd(commands["RPM"]), obd.cmd(commands["ENGINE_LOAD"]), obd.cmd(commands["COOLANT_TEMP"])
+    except KeyboardInterrupt:
+        print sl >> obd.cmd(commands["SPEED"])
+        raise
 
 # while True:
 #     print obd.cmd(commands["SPEED"]), obd.cmd(commands["RPM"])
