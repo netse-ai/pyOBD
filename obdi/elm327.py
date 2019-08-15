58641a7229dc217748b0bac9069ed3be54047932
Trent Resh <tear728@gmail.com>
2019-08-15 14:07:03 -0700

    testing bt connection

--- obdi/elm327.py
+++ obdi/elm327.py
@@ -31,7 +31,7 @@ class ELM327(object):
         #     print "sudo rfcomm bind rfcomm0 " + bd_addr == "sudo rfcomm bind rfcomm0 00:1D:A5:02:86:67"
         #
         try:
-                stat.S_ISBLK(os.stat("/dev/rfcomm0").st_mode)
+            stat.S_ISBLK(os.stat("/dev/rfcomm0").st_mode)
         except:
             os.system("sudo rfcomm bind rfcomm0 00:1D:A5:02:86:67")
         try:
