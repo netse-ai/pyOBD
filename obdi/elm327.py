09f17cf9c9920fe11eae4caff178d6a3401409c6
Trent Resh <tear728@gmail.com>
2019-08-15 14:06:15 -0700

    testing bt connection

--- obdi/elm327.py
+++ obdi/elm327.py
@@ -3,6 +3,7 @@ import time
 import threading
 import os
 import stat
+import bluetooth
 from commands import commands
 from serial import SerialException
 
@@ -14,25 +15,25 @@ class ELM327(object):
         self.threads = []
 
     def connect(self):
-        
-        print "Attempting to Connect"
-        try:
-            stat.S_ISBLK(os.stat("/dev/rfcomm0").st_mode)
-        except:
-            nearby_devices = bluetooth.discover_devices(lookup_names=True)
-            print "NEAR BY DEVICES?"
-            bd_addr = None
-            for addr, name  in nearby_devices:
-                print addr, name
-                if name == "OBDII":
-                    bd_addr = addr
-            os.system("sudo rfcomm bind rfcomm0 " + bd_addr)
-            print "sudo rfcomm bind rfcomm0 " + bd_addr == "sudo rfcomm bind rfcomm0 00:1D:A5:02:86:67"
 
+        print "Attempting to Connect"
         # try:
-        #         stat.S_ISBLK(os.stat("/dev/rfcomm0").st_mode)
+        #     stat.S_ISBLK(os.stat("/dev/rfcomm0").st_mode)
         # except:
-        #     os.system("sudo rfcomm bind rfcomm0 00:1D:A5:02:86:67")
+        #     nearby_devices = bluetooth.discover_devices(lookup_names=True)
+        #     print "NEAR BY DEVICES?"
+        #     bd_addr = None
+        #     for addr, name  in nearby_devices:
+        #         print addr, name
+        #         if name == "OBDII":
+        #             bd_addr = addr
+        #     os.system("sudo rfcomm bind rfcomm0 " + bd_addr)
+        #     print "sudo rfcomm bind rfcomm0 " + bd_addr == "sudo rfcomm bind rfcomm0 00:1D:A5:02:86:67"
+        #
+        try:
+                stat.S_ISBLK(os.stat("/dev/rfcomm0").st_mode)
+        except:
+            os.system("sudo rfcomm bind rfcomm0 00:1D:A5:02:86:67")
         try:
             self.ser = serial.Serial('/dev/rfcomm0', baudrate=self.baudrate, timeout=self.timeout)
             if self.ser.isOpen():
