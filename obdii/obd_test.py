140df577c36bbc20c2e70bfb5df8254f79955ad9
Trent Resh <tear728@gmail.com>
2019-11-26 13:29:13 -0800

    firebase

--- obdii/obd_test.py
+++ obdii/obd_test.py
@@ -33,36 +33,36 @@ cmds = {
 
 from firebase import firebase
 firebase = firebase.FirebaseApplication('https://planit-a12ac.firebaseio.com', None)
+new_user = 'boost'
+
+result = firebase.post('/obdii', new_user, {'print': 'pretty'}, {'X_FANCY_HEADER': 'VERY FANCY'})
+print result
 
 
+# while True:
+#     try:
+#         responses = obd.interface.multi_commands(**cmds)
+#         # speed = responses['SPEED']
+#         # rpm = responses['RPM']
+#         # engine_load = responses['ENGINE_LOAD']
+#         # coolant_tmp = responses['COOLANT_TEMP']
+#         intake_pressure = responses['INTAKE_PRESSURE']
+#         barometric_pressure = responses['BAROMETRIC_PRESSURE']
+#         boost_pressure = str((intake_pressure - float(barometric_pressure))/6.895)[0:6]
+#         # tq = responses['BAROMETRIC_PRESSURE']
+#         # print "SPEED\t RPM\t ENGINE_LOAD\t COOLANT_TEMP\t BOOST\t"
+#         # print speed, "\t", rpm, "\t", engine_load, "\t", coolant_tmp, "\t\t", boost_pressure, " **** ", str(float(boost_pressure) / 14.504)[0:6]
+#         print "Boost PSI    -------    Boost BARS"
+#         print boost_pressure + "       -------    " + str(float(boost_pressure) / 14.504)[0:6]
+#         # print >> sl, float(speed)
+#         # print >> rl, float(rpm)
+#         # print >> el, float(engine_load)
+#         # print >> cl, float(coolant_tmp)
+#         print >> boost, float(boost_pressure)
 
-while True:
-    try:
-        responses = obd.interface.multi_commands(**cmds)
-
-        # speed = responses['SPEED']
-        # rpm = responses['RPM']
-        # engine_load = responses['ENGINE_LOAD']
-        # coolant_tmp = responses['COOLANT_TEMP']
-        intake_pressure = responses['INTAKE_PRESSURE']
-        barometric_pressure = responses['BAROMETRIC_PRESSURE']
-        boost_pressure = str((intake_pressure - float(barometric_pressure))/6.895)[0:6]
-        # tq = responses['BAROMETRIC_PRESSURE']
-        # print "SPEED\t RPM\t ENGINE_LOAD\t COOLANT_TEMP\t BOOST\t"
-        # print speed, "\t", rpm, "\t", engine_load, "\t", coolant_tmp, "\t\t", boost_pressure, " **** ", str(float(boost_pressure) / 14.504)[0:6]
-        print "Boost PSI    -------    Boost BARS"
-        print boost_pressure + "       -------    " + str(float(boost_pressure) / 14.504)[0:6]
-        # print >> sl, float(speed)
-        # print >> rl, float(rpm)
-        # print >> el, float(engine_load)
-        # print >> cl, float(coolant_tmp)
-        print >> boost, float(boost_pressure)
-        result = firebase.post('/obdii', boost, {'print': 'pretty'}, {'X_FANCY_HEADER': 'VERY FANCY'})
-
-
-    except KeyboardInterrupt:
-        print "\nFinishing..."
-        sys.exit(0)
+#     except KeyboardInterrupt:
+#         print "\nFinishing..."
+#         sys.exit(0)
 
 # #TODO: threading should make this process fast. Currently slows by a factor of ELM327.timeout for every command
 # # while True:
