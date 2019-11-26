407d8842ea2816534f92e75850f0d03244f8069b
Trent Resh <tear728@gmail.com>
2019-11-26 14:08:46 -0800

    firebase testing

--- obdii/obd_test.py
+++ obdii/obd_test.py
@@ -6,17 +6,17 @@ import bluetooth
 from obd import OBDII
 from commands import commands
 
-
+#access firebase instance
 from firebase import firebase
 firebase = firebase.FirebaseApplication('https://planit-a12ac.firebaseio.com', None)
-
+#/obdii/boost child
+data = '-LudfF76bof48zB80laR'
 
 #create an OBDI instance
 obd = OBDII(baudrate=115200, timeout=0.175)
+
 #connect to elm327
 obd.connect()
-
-boost = open('boost_pressure.txt', 'w')
 
 #create a commands object to be passed to obd
 cmds = {
@@ -27,16 +27,23 @@ cmds = {
 
 while True:
     try:
+        #get obdii responses
         responses = obd.interface.multi_commands(**cmds)
+
+        #intake pressure and barometric pressure readings
         intake_pressure = responses['INTAKE_PRESSURE']
         barometric_pressure = responses['BAROMETRIC_PRESSURE']
+
+        #caluclate boost pressure
         boost_pressure = str((intake_pressure - float(barometric_pressure))/6.895)[0:6]
+        #convert to bars
+        boost_bars = str(float(boost_pressure) / 14.504)[0:6]
         print "Boost PSI    -------    Boost BARS"
-        print boost_pressure + "       -------    " + str(float(boost_pressure) / 14.504)[0:6]
-        print >> boost, float(boost_pressure)
-        data = '-LudfF76bof48zB80laR'
+        print boost_pressure + "       -------    " + boost_bars
+
+        #write boost readings to firebasse
         snapshot = firebase.put('/obdii/boost', data, {'boost PSI': boost_pressure, 'boost bars' : str(float(boost_pressure) / 14.504)[0:6]}, {'X_FANCY_HEADER': 'VERY FANCY'})
-        print snapshot
+    
     except KeyboardInterrupt:
         print "\nFinishing..."
         sys.exit(0)
